ca54477c2252a5815b761991ef7181ec4ac59e43
wildananshari <87628773+wildananshari@users.noreply.github.com>
2024-05-05 20:40:16 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Puput Intan WEB</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="index.php">BERANDA</a> 
+        <a class="nav-link active" aria-current="page" href="portofolio.php">PORTOFOLIO</a> 
+        <a class="nav-link active" aria-current="page" href="pelatihan.php">PELATIHAN</a> 
+        <a class="nav-link active" aria-current="page" href="pendidikan.php">PENDIDIKAN</a> 
+        <a class="nav-link active" aria-current="page" href="biodata.php">BIODATA</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+    <hr />
+
+    <header style="text-align: center;">
+        <img src="image/foto-profile.jpeg" width="200" height="200" style="border-radius: 50%;"/>
+        <h1>Puput Intan</h1>
+        <p>(MAHASISWA MAGANG)</p>
+    </header>
+
+    <hr />
+
+    <article style="text-align: center;">
+        <h2>Biodata</h2>
+        <p>
+            Halo, Nama Saya Puput Intan saya seorang mahasiswa di kampus STMIK DCI
+        </p>
+        <p>
+            <video controls width="400">
+                <source src="video/video-about.mp4" type="video/mp4"/>
+            </video>
+        </p>
+    </article>
+    <hr>
+    <footer style="text-align: center;">
+        <p>Copyright &copy; 2024 Puput Intan.</p>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Puput Intan WEB</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="index.php">BERANDA</a> 
+        <a class="nav-link active" aria-current="page" href="portofolio.php">PORTOFOLIO</a> 
+        <a class="nav-link active" aria-current="page" href="pelatihan.php">PELATIHAN</a> 
+        <a class="nav-link active" aria-current="page" href="pendidikan.php">PENDIDIKAN</a> 
+        <a class="nav-link active" aria-current="page" href="biodata.php">BIODATA</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+    <hr />
+
+    <div class="pic-car">
+        <img src="image/banner.png" alt="banner promo" class="pic" width="100%" height="50%">
+    </div>
+
+    <hr />
+
+    <hr>
+    <footer style="text-align: center;">
+        <p>Copyright &copy; 2024 Puput Intan.</p>
+    </footer>
+</body>
+</html>
